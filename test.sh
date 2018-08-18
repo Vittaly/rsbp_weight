720ed54dc08d3b0aff1d44785d334557d33512b5
vitaly.burkut <vitaly.burkut@gmail.com>
2018-08-19 04:46:54 +0500

    update complete

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
-for i in {1..100}; do echo 10 kg > /tmp/ttyS101; sleep .1; echo $i;  done
-for i in {1..100}; do echo 11 kg > /tmp/ttyS101; sleep .1; echo $i;  done
-for i in {1..100}; do echo 13 kg > /tmp/ttyS101; sleep .1; echo $i;  done
-for i in {1..100}; do echo 14 kg > /tmp/ttyS101; sleep .1; echo $i;  done
+#bash
+cnt=20
+for i in $(eval echo {1..$cnt}); do echo 10 kg > /tmp/ttyS101; sleep .3; echo $i;  done
+for i in $(eval echo {1..$cnt}); do echo 11 kg > /tmp/ttyS101; sleep .3; echo $i;  done
+for i in $(eval echo {1..$cnt}); do echo 3.01 kg > /tmp/ttyS101; sleep .3; echo $i;  done
+for i in $(eval echo {1..$cnt}); do echo 3.02 kg > /tmp/ttyS101; sleep .3; echo $i;  done
+for i in $(eval echo {1..$cnt}); do echo 2.99 kg > /tmp/ttyS101; sleep .3; echo $i;  done
+for i in $(eval echo {1..$cnt}); do echo 3 kg > /tmp/ttyS101; sleep .3; echo $i;  done
+for i in $(eval echo {1..$cnt}); do echo 1 kg > /tmp/ttyS101; sleep .3; echo $i;  done
+for i in $(eval echo {1..$cnt}); do echo 14 kg > /tmp/ttyS101; sleep .3; echo $i;  done
